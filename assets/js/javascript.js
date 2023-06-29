eab6298afe2ea51c3fa282e5ad3154c3c5bab108
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-06-29 16:34:10 +0100

    Added: basic structure of game functions

---
+++ assets/js/javascript.js
@@ -0,0 +1,23 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
