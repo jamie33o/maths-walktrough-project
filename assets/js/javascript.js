403945e49b24727dae5a21663932d7bfba78bd3b
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-06-30 16:25:32 +0100

    add code to check if answer is right

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit")
+                checkAnswer();
             }else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -34,7 +34,23 @@ function runGame(gameType) {
 
 }
 
+/**
+ * Checks the answer against the first element in
+ * the returned calculatecorrectanswer array
+ */
 function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    }else {
+        alert(`Awww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`)
+    }
+
+    runGame(calculatedAnswer[1])
 
 }
 
@@ -47,7 +63,7 @@ function calculateCorrectAnswer() {
 
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand1').innerText);
-    let operand3 = document.getElementById('operator').innerText;
+    let operator = document.getElementById('operator').innerText;
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
@@ -68,7 +84,7 @@ function incrementWrongAnswer() {
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById(`operand1`).textContent = operand1;
     document.getElementById(`operand2`).textContent = operand2;
-    document.getElementById(`operand3`).textContent = "+";
+    document.getElementById(`operator`).textContent = "+";
 
 
 }
